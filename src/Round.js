aa623b654dd8a45d566434a6078d57d267fdea2b
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-08-17 18:52:01 -0600

    Add takeTurn method, need to complete

--- src/Round.js
+++ src/Round.js
@@ -5,6 +5,19 @@ const util = require('./util');
 class Round {
   constructor(deck) {
     this.deck = deck.cardsInDeck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+
+  }
+
+  returnCurrentCard() {
+    return this.deck[0];
+  }
+
+  taketurn() {
+    var newTurn = new Turn;
+    this.turns++;
+    
   }
 
 }
--- test/Round-test.js
+++ test/Round-test.js
@@ -7,7 +7,7 @@ const Round = require('../src/Round');
 
 describe('Round', function() {
 
-  it('should be initialized with an array of Card objects', function() {
+  it('should return cards in deck', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
@@ -19,7 +19,17 @@ describe('Round', function() {
     expect(round.deck).to.equal(deck.cardsInDeck);
   });
 
+  it('should return current card', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
 
+    const deck = new Deck([card1, card2, card3]);
+
+    const round = new Round(deck);
+
+    expect(round.returnCurrentCard()).to.equal(card1);
+  });
 
 
 
